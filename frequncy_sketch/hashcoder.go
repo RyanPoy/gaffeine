7568f34acd59c28b8737d5f73233ab2776d9b53f
pengyi <ryanpoy@gmail.com>
2024-10-10 09:11:10 +0800

    update StringHashCoder

--- frequncy_sketch/hashcoder.go
+++ frequncy_sketch/hashcoder.go
@@ -8,25 +8,32 @@ type HashCoder[K Key] interface {
 	HashCode(v K) uint32
 }
 
-type IntegerHashCoder[K Key] struct {
-}
+type IntegerHashCoder[K Key] struct{}
 
 func (h *IntegerHashCoder[K]) HashCode(v K) uint32 {
 	return uint32(v)
 }
 
-type Float32HashCoder[K Key] struct {
-}
+type Float32HashCoder[K Key] struct{}
 
 func (h *Float32HashCoder[K]) HashCode(v K) uint32 {
 	return math.Float32bits(float32(v))
 }
 
-type Float64HashCoder[K Key] struct {
-}
+type Float64HashCoder[K Key] struct{}
 
 func (h *Float64HashCoder[K]) HashCode(v K) uint32 {
 	bits := math.Float64bits(float64(v))
 	hash := bits ^ (bits >> 32) // 与 Java 的哈希计算相同
 	return uint32(hash)
+}
+
+type StringHashCoder[K Key] struct{}
+
+func (h *StringHashCoder[K]) HashCode(v K) uint32 {
+	r := 0
+	for _, b := range any(v).(string) {
+		r = 31*r + int(b)
+	}
+	return uint32(r)
 }
